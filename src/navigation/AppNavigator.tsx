65db3f627001950f53cba3b558768279da357b23
nat10marc <natanmarc95777@gmail.com>
2024-09-25 09:36:21 -0300

    atualizando appnavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from '../screens/WelcomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import SignUpScreen from '../screens/SignUpScreen';
-import ConsultationsListScreen from '../screens/ConsultationsListScreen';
-import ScheduleConsultationScreen from '../screens/ScheduleConsultationScreen';
+import React from 'react';
 import ConfirmAppointmentScreen from '../screens/ConfirmAppointmentScreen';
+import ConsultationsListScreen from '../screens/ConsultationsListScreen';
+import LoginScreen from '../screens/LoginScreen';
+import ScheduleConsultationScreen from '../screens/ScheduleConsultationScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 // Definindo o RootStackParamList com todas as telas do projeto
 export type RootStackParamList = {
@@ -22,12 +22,36 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="ConsultationsList" component={ConsultationsListScreen} />
-      <Stack.Screen name="ScheduleConsultation" component={ScheduleConsultationScreen} />
-      <Stack.Screen name="ConfirmAppointment" component={ConfirmAppointmentScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{ title: 'Teste' }} // Título personalizável
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ title: 'Acesse sua conta' }} // Título personalizável
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUpScreen}
+        options={{ title: 'Cadastrar' }} // Título personalizável
+      />
+      <Stack.Screen
+        name="ConsultationsList"
+        component={ConsultationsListScreen}
+        options={{ title: 'Consultas Agendadas' }} // Título personalizável
+      />
+      <Stack.Screen
+        name="ScheduleConsultation"
+        component={ScheduleConsultationScreen}
+        options={{ title: 'Agendar Consulta' }} // Título personalizável
+      />
+      <Stack.Screen
+        name="ConfirmAppointment"
+        component={ConfirmAppointmentScreen}
+        options={{ title: 'Confirmação de Agendamento' }} // Título personalizável
+      />
     </Stack.Navigator>
   );
 };
